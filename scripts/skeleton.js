86ea5c8dbde55182a946aeb4c3bd3d6e030a4fed
Jaskirat <jsingh573@my.bcit.ca>
2023-03-05 16:36:53 -0800

    update navbar,main page and create logo,firestore users collection.

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,20 +1,21 @@
-//---------------------------------------------------
-// This function loads the parts of your skeleton 
-// (navbar, footer, and other things) into html doc. 
-//---------------------------------------------------
-// function loadSkeleton(){
-//     console.log($('#navbarPlaceholder').load('./text/nav.html'));
-//     console.log($('#footerPlaceholder').load('./text/footer.html'));
-// }
-// loadSkeleton();  //invoke the function
-
 
 // Injects the navbar, and footer into the HTML doc
-function injectTemplates() {
-    console.log($('#nav').load('text/nav.html'));
-    console.log($('#foot').load('text/footer.html'));
-}
 
-// Calls the inject_navfoot function
-injectTemplates();
+function loadSkeleton() {
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            // Do something for the user here.
+            console.log($('#nav').load('text/nav_after_login.html'));
+            console.log($('#foot').load('text/footer.html'));
+        } else {
+            // No user is signed in.
+            console.log($('#nav').load('text/nav_before_login.html'));
+            console.log($('#foot').load('text/footer.html'));
+        }
+    });
+}
+loadSkeleton(); //invoke the function
+
 
